7104af5b944508a65e12beab56433e1f18105830
Neil Kenneth A. Genotiva <genotiva.nka@ad.sprobe.ph>
2022-10-06 14:41:12 +0800

    first commit

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Item from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+
+export const ExpenseList = ({
+  expenses,
+  handleEdit,
+  handleDelete,
+  clearItems,
+}) => {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return (
+            <Item
+              key={expense.id}
+              expense={expense}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+            />
+          );
+        })}
+      </ul>
+      {expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+          Clear Expenses
+          <MdDelete className="btn-icon" />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ExpenseList;
